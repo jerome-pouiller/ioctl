be5f348a55cc6d3a53ddd7b4928b2de9e3d215f4
Jérôme Pouiller <jezz@sysmic.org>
2015-12-17 17:55:56 +0100

    Some ioctls was missing

--- gen_ioctls_list.sh
+++ gen_ioctls_list.sh
@@ -14,9 +14,14 @@ EXCLUDE_FILES+=" --exclude auto_fs.h"           # compat_ulong_t undeclared
 EXCLUDE_FILES+=" --exclude coda.h"              # Include linux/time.h which conflict with time.h
 # EXCLUDE_FILES+=" --exclude if_ppp.h"          # net/if_ppp.h conflict with linux/ppp-ioctl.h
 EXCLUDE_FILES+=" --exclude ppp-ioctl.h"         # linux/ppp-ioctl.h conflict with net/if_ppp.h
+EXCLUDE_FILES+=" --exclude atmbr2684.h"         # Include linux/if.h which conflict with net/if.h
+EXCLUDE_FILES+=" --exclude mtio.h"              # sys/mtio.h conflict with linux/mtio.h
+EXCLUDE_FILES+=" --exclude raw.h"               # sys/raw.h conflict with linux/raw.h
 EXCLUDE_FILES+=" --exclude ioctl.h"             # Does not contains any ioctl in fact
 EXCLUDE_FILES+=" --exclude mount.h"             # sys/mount.h conflict with linux/fs.h
 EXCLUDE_FILES+=" --exclude asequencer.h"        # Lack of type definition
+EXCLUDE_FILES+=" --exclude if_pppol2tp.h"       # Lack of type definition
+EXCLUDE_FILES+=" --exclude if_pppox.h"          # Lack of type definition
 EXCLUDE_FILES+=" --exclude-dir Peripherals"     # Old versions of linux/fmd/Peripherals/ missed many files
 EXCLUDE_IOCTLS+=" -e USBDEVFS_CONTROL32"        # Lack type size
 EXCLUDE_IOCTLS+=" -e USBDEVFS_BULK32"           # Lack type size
@@ -30,9 +35,11 @@ EXCLUDE_IOCTLS+=" -e KVM_PPC_RTAS_DEFINE_TOKEN" # Lack type size
 EXCLUDE_IOCTLS+=" -e KVM_ARM_VCPU_INIT"         # Lack type size
 EXCLUDE_IOCTLS+=" -e KVM_ARM_PREFERRED_TARGET"  # Lack type size
 EXCLUDE_IOCTLS+=" -e BLKTRACESETUP"             # Lack type size
+EXCLUDE_IOCTLS+=" -e FS_IOC_FIEMAP"             # Lack type size
 EXCLUDE_IOCTLS+=" -e BTRFS_IOC_SET_FSLABEL"     # Lack type size
 EXCLUDE_IOCTLS+=" -e BTRFS_IOC_GET_FSLABEL"     # Lack type size
 EXCLUDE_IOCTLS+=" -e BTRFS_IOC_DEFRAG_RANGE"    # Lack type size
+EXCLUDE_IOCTLS+=" -e COMPAT_ATM_ADDPARTY"       # Parsing error
 EXCLUDE_IOCTLS+=" -e BLKELVGET"                 # Inside #if 0
 EXCLUDE_IOCTLS+=" -e BLKELVSET"                 # Inside #if 0
 
@@ -45,24 +52,25 @@ EXCLUDE_IOCTLS+=" -e BLKELVSET"                 # Inside #if 0
 echo '#include <unistd.h>'  # size_t
 echo '#include <net/if.h>'  # IFNAMSIZ at linux/isdn_ppp.h:38
 echo '#include <stdint.h>'  # For alsa headers
-# echo '#include <time.h>'    # alsa headers
-# echo '#include <termios.h>' # struct termios2
+echo '#include <asm/termbits.h>' # struct termios2
+# Place here your extra headers
 echo '#include "ioctls_list.h"'
-grep -lr $EXCLUDE_FILES --exclude-dir \*-linux-\* '^#define[^(].*[ \t]_IO[RW]*(' "$SYSROOT/usr/include" |
+grep -lr $EXCLUDE_FILES --exclude-dir \*-linux-\* '^#define[^(].*[ 	]_IO[RW]*(' "$SYSROOT/usr/include" |
     sed -e "s|$SYSROOT/usr/include/\(.*\)|#include <\1>|"
 if [ -n $ARCH ]; then
-    grep -lr $EXCLUDE_FILES '^#define[^(].*[ \t]_IO[RW]*(' "$SYSROOT/usr/include/$ARCH" |
+    grep -lr $EXCLUDE_FILES '^#define[^(].*[ 	]_IO[RW]*(' "$SYSROOT/usr/include/$ARCH" |
         sed -e "s|$SYSROOT/usr/include/$ARCH/\(.*\)|#include <\1>|"
 fi
 echo
 echo "const struct ioctl_entry ioctls_list[] = {"
-grep -nr $EXCLUDE_FILES --exclude-dir \*-linux-\* '^#define[^(]*[ \t]_IO[RW]*(' "$SYSROOT/usr/include" |
+grep -nr $EXCLUDE_FILES --exclude-dir \*-linux-\* '^#define[^(]*[ 	]_IO[RW]*(' "$SYSROOT/usr/include" |
     grep -v $EXCLUDE_IOCTLS |
-    sed -e "s|$SYSROOT/usr/include/\(.*:.*\):#define[ \t]*\([A-Z0-9x_]*\).*|    { \"\2\", \2, -1, -1 }, // \1|"
+    sed -e "s|$SYSROOT/usr/include/\(.*:.*\):#define[ 	]*\([A-Z0-9x_]*\).*|    { \"\2\", \2, -1, -1 }, // \1|"
 if [ -n $ARCH ]; then
     grep -nr $EXCLUDE_FILES '^#define[^(]*[ \t]_IO[RW]*(' "$SYSROOT/usr/include/$ARCH" |
         grep -v $EXCLUDE_IOCTLS |
-        sed -e "s|$SYSROOT/usr/include/\(.*:.*\):#define[ \t]*\([A-Z0-9x_]*\).*|    { \"\2\", \2, -1, -1 }, // \1|"
+        sed -e "s|$SYSROOT/usr/include/\(.*:.*\):#define[ 	]*\([A-Z0-9x_]*\).*|    { \"\2\", \2, -1, -1 }, // \1|"
 fi
+# Place here you extra entries
 echo "    { NULL, 0 },"
 echo "};"
